b06806cbc6f1a268ba7f6382df87b9a9b0caee76
MolokoMakgolane <makgolanebatlilemoloko@gmail.com>
2025-03-20 14:19:52 +0200

    Add files via upload

---
+++ blogging_platform/confessions_of_an_undercover_yapper/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ConfessionsOfAnUndercoverYapperConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'confessions_of_an_undercover_yapper'
